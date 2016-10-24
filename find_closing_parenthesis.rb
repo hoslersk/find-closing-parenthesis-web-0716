5de188f1810113244d0a548d0f85eade237b85ae
Schyler Hosler <schylerhosler@gmail.com>
2016-10-24 16:28:05 -0400

    Ruby practice with locating ending parenthesis in string of parentheses... all tests passing (need to refactor)

--- find_closing_parenthesis.rb
+++ find_closing_parenthesis.rb
@@ -1,3 +1,23 @@
+require 'pry'
 def find_closing_parenthesis(sentence, open_position)
-
+  counter = 0
+  open_p_count = 0
+  # close_p_count = 0
+  while counter < sentence.length do
+    if counter >= open_position
+      if sentence[counter] == "("
+        open_p_count += 1
+        # counter += 1
+      elsif sentence[counter] == ")"
+        # close_p_count += 1
+        if open_p_count == 1
+          return counter
+        else
+          open_p_count -= 1
+        end
+        # counter += 1
+      end
+    end
+    counter += 1
+  end
 end
